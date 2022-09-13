18c23b3d76ec1f152d5eacbfc754207a259e6720
Gabriel Badea <gabriel.badea@soft-tehnica.com>
2022-09-13 22:34:17 +0300

    Grid somewhat works

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 'use strict';
 
-const header = document.querySelector('#header');
+const container = document.querySelector('.grid');
 
-header.textContent = 'Jef!';
+let gridSize = 16;
+
+// Initialize
+generateGrid(gridSize);
+
+function generateGrid(gridSize) {
+    const containerSide = container.offsetWidth;
+    console.log(containerSide);
+    console.log(containerSide / gridSize);
+    let sizes = '';
+    for (let i = 0; i < containerSide / gridSize; i++) {
+        sizes += gridSize + 'px ';
+    }
+    console.log(sizes);
+    container.style.gridTemplateColumns = sizes;
+    container.style.gridTemplateRows = sizes;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('grid-item');
+        pixel.addEventListener('mouseenter', () => {
+            pixel.classList.add = 'hover';
+        });
+        container.appendChild(pixel);
+    }
+}
+
+function setGridSize(size) {
+    gridSize = size;
+}
